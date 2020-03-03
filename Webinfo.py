9afd017929e2423ed530be1a536b387300b75ab0
TjerkvGerwen <tjerkvangerwen@gmail.com>
2020-03-03 13:59:33 +0100

    incl port scan

--- Webinfo.py
+++ Webinfo.py
@@ -1,31 +1,40 @@
 #Web info
-#website url opvragen van:
 #- Nameservers - Check
 #- CNAME -
-#- IP
 #- Certificaat
 #- Mailserver
-#- SPF record
-#- txt records
+
 counternameserver = 0
 nameserverlist = []
-
-
+port = 80
+import socket
 import dns.resolver
 domain = input ("Wat is het domein naam?: ")
+portscan = input("Wilt u ook de meest standaard open poorten checken? (ja/nee)")
+
+if (portscan == "ja" or portscan == "Ja" or portscan == "JA" or portscan == "J" or portscan == "j"):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((domain, port))
+    if result == 0:
+        print ("port",port ,"is open")
+    else:
+        print("port",port ,"is not open")
+    sock.close()
+
+
+ip = socket.gethostbyname(domain)
+print("Dit is het IP adress: ",ip)
 
 #Opvragen van nameserver
 nameservers = dns.resolver.query(domain,'NS')
 for server in nameservers:
     counternameserver = counternameserver + 1
     nameserverlist.insert(counternameserver,server.target)
+print("Dit zijn de DNS nameservers: ", nameserverlist)
 
 # TXT record
-answers = dns.resolver.query('google.com', 'TXT')
-print(' query qname:', answers.qname, ' num ans.', len(answers))
+answers = dns.resolver.query(domain, 'TXT')
 for rdata in answers:
     for txt_string in rdata.strings:
-      print (' TXT:', txt_string)
+      print ('Dit is het TXT record(s): ', txt_string)
 
-
-print("de DNS nameservers zijn: ",nameserverlist)
